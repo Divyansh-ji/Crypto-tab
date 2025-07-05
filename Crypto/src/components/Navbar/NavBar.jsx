c5847e4501352ab3175925951fd4519ec9afbd32
Divyansh-ji <Divyanshtiwary01@gmail.com>
2025-07-05 10:00:39 +0530

    Just Updated Api today

--- Crypto/src/components/Navbar/NavBar.jsx
+++ Crypto/src/components/Navbar/NavBar.jsx
@@ -6,26 +6,26 @@ import { CoinContext } from "../../context/CoinContext";
 
 const NavBar = () => {
   const { setCurrency } = useContext(CoinContext);
+
   const currencyHandler = (event) => {
     switch (event.target.value) {
-      case "usd": {
+      case "usd":
         setCurrency({ name: "usd", Symbol: "$" });
         break;
-      }
-      case "eur": {
-        setCurrency({ name: "eur", Symbol: "&" });
+      case "eur":
+        setCurrency({ name: "eur", Symbol: "€" });
         break;
-      }
-      case "inr": {
-        setCurrency({ name: "inr", Symbol: "RU" });
+      case "inr":
+        setCurrency({ name: "inr", Symbol: "₹" });
         break;
-      }
+      default:
+        break;
     }
   };
 
   return (
     <div className="navbar">
-      <img src={Logo} alt="" className="logo" />
+      <img src={Logo} alt="Logo" className="logo" />
       <ul>
         <li>Home</li>
         <li>Feature</li>
@@ -33,13 +33,13 @@ const NavBar = () => {
         <li>Blog</li>
       </ul>
       <div className="nav-right">
-        <select>
+        <select onChange={currencyHandler}>
           <option value="usd">USD</option>
           <option value="eur">EUR</option>
           <option value="inr">INR</option>
         </select>
         <button>
-          Sign up <img src={arrow_icon} alt="" />
+          Sign up <img src={arrow_icon} alt="arrow icon" />
         </button>
       </div>
     </div>
--- Crypto/src/pages/Home/Home.jsx
+++ Crypto/src/pages/Home/Home.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./Home.css";
+import { CoinContext } from "../../context/CoinContext";
 const Home = () => {
+  const { allcoin, currency } = useContext(CoinContext);
+  const [displayCoin, setDisplayCoin] = useState([]);
+  useEffect(() => {
+    setDisplayCoin(allcoin);
+  }, [allcoin]);
   return (
     <div className="home">
       <div className="hero">
@@ -25,6 +31,20 @@ const Home = () => {
           <p style={{ textAlign: "centre" }}>24H Change</p>
           <p className="market-cap">Market Cap</p>
         </div>
+        {displayCoin.slice(0, 10).map((item, index) => {
+          return (
+            <div className="tabel-layout" key={index}>
+              <p>{item.market_cap_rank}</p>
+              <div>
+                <img src={item.image} alt="" />
+                <p>{item.name + "-" + item.symbol}</p>
+              </div>
+              <p>
+                {currency.symbol} {item.current_price}
+              </p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
